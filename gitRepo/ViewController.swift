d5d4fb971a74c5c8681bff0e84cecfbffbc963c5
Hamiyet Özdemir <hmytt.9@gmail.com>
2021-04-06 17:19:10 +0300

    helloFunction

--- gitRepo/ViewController.swift
+++ gitRepo/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    let hello="hello world"
+    let key = "This is Hamiyet"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print(hello)
     }
 
 
