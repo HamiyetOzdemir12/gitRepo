0f4ab54e5817cd679306c75ab349baf109b8eb3d
Hamiyet Özdemir <hmytt.9@gmail.com>
2021-08-02 12:49:35 +0300

    deneme-2

--- gitRepo/ViewController.swift
+++ gitRepo/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     let hello="hello world"
     let key = "This is Hamiyet"
     let deneme = "merge denemesi"
+    let deneme2 = "merge denemesi-2"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
